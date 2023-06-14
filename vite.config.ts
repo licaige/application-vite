6a122fc7db0350a3627762dc1f4cffcf7a35dee5
“李琦” <qi.li@hongpucorp.com>
2023-06-14 14:43:19 +0800

    fix:自动样式插件引入

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,36 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+// element-plus组件按需引入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 自动引入element-plus的style
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `element-plus/theme-chalk/${name}.css`
+          }
+        }
+      ]
+    })
   ],
   resolve: {
+    // 别名
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
