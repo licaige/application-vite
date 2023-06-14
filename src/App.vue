802f3bb8f8706361c01e0c59552e01e6216b75b0
“李琦” <qi.li@hongpucorp.com>
2023-06-14 17:19:43 +0800

    fix(初始化): 配置

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div class="app">
     <el-button type="primary">点击试试</el-button>
     <div>你愁啥</div>
+    <div>dsafdsafd</div>
     <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto distinctio sint
-      recusandae dicta accusamus aspernatur ex culpa libero similique, reiciendis necessitatibus
-      repellat quis repellendus quas. Harum enim nisi facilis expedita.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto doloremque quaerat alias.
+      Dicta, laudantium placeat. Provident fugit temporibus ratione asperiores esse ducimus
+      reiciendis sequi magni rerum tempora incidunt, iste iure!
     </div>
+    <div>dsfasdfd</div>
   </div>
 </template>
 <script setup lang="ts"></script>
