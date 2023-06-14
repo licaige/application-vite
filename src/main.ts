078cd187d83da37530bc3b9e9513065eddc3ed25
“李琦” <qi.li@hongpucorp.com>
2023-06-14 14:56:46 +0800

    fix:全局注册图标

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
+import './assets/main.css'
+// 样式重置
+import 'normalize.css'
 const app = createApp(App)
+// 导入所有图标并进行全局注册
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
