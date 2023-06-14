b715b961ebec957bd74d094fda6235f13616d785
“李琦” <qi.li@hongpucorp.com>
2023-06-14 17:04:55 +0800

    你猜

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app">
     <el-button type="primary">点击试试</el-button>
+    <div>你愁啥</div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto distinctio sint
+      recusandae dicta accusamus aspernatur ex culpa libero similique, reiciendis necessitatibus
+      repellat quis repellendus quas. Harum enim nisi facilis expedita.
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
