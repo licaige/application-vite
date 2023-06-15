33f9043fd1599ef4766ac3a2bacb299c2c620481
“李琦” <qi.li@hongpucorp.com>
2023-06-15 18:05:20 +0800

    feat(svg): icon图标

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,12 @@
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
-
+// @vue/prettier/@typescript-eslint
 module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
-    "@vue/prettier/@typescript-eslint",
     "@vue/eslint-config-prettier/skip-formatting",
   ],
   parserOptions: {
--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElTooltip: typeof import('element-plus/es')['ElTooltip']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
