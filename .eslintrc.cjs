46c52f8d518fb5b9d63fd1161dca81553a6aa385
“李琦” <qi.li@hongpucorp.com>
2023-06-15 11:03:48 +0800

    fix:回滚

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'vue/valid-v-slot': [
-      'error',
-      {
-        allowModifiers: true
-      }
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'no-irregular-whitespace': 'off',
-    'vue/no-mutating-props': 'off'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
   <div class="app">
     <el-button type="primary">点击试试</el-button>
     <div>你愁啥</div>
-    <div>dsafdsafd</div>
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto doloremque quaerat alias.
-      Dicta, laudantium placeat. Provident fugit temporibus ratione asperiores esse ducimus
-      reiciendis sequi magni rerum tempora incidunt, iste iure!
-    </div>
-    <div>dsfasdfd</div>
   </div>
 </template>
 <script setup lang="ts"></script>
