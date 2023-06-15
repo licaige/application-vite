22a9deaf3d67170d9eb3a06a434821f0a11fa05c
“李琦” <qi.li@hongpucorp.com>
2023-06-15 11:15:51 +0800

    fix:归类

--- .prettier.cjs
+++ .prettier.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   singleQuote: true, // 使用单引号
   printWidth: 80, // 超过最大值换行
-  htmlWhitespaceSensitivity: "ignore",
+  htmlWhitespaceSensitivity: 'ignore',
   semi: false, // 结尾不用分号
-  trailingComma: "none", // 函数最后不需要逗号
-  arrowParens: "avoid", // 箭头函数参数只有一个时，不需要括号
+  trailingComma: 'none', // 函数最后不需要逗号
+  arrowParens: 'avoid', // 箭头函数参数只有一个时，不需要括号
   bracketSpacing: true, // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
   jsxBracketSameLine: false, // 在jsx中把'>' 是否单独放一行
   jsxSingleQuote: true, // 在jsx中使用单引号代替双引号
-  proseWrap: "preserve", //代码超出是否要换行 preserve保留
-};
+  proseWrap: 'preserve' //代码超出是否要换行 preserve保留
+}
--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="app">
     <el-button type="primary">点击试试</el-button>
     <div>你愁啥</div>
+    <div>nik</div>
   </div>
 </template>
 <script setup lang="ts"></script>
