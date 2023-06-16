125be6d3dd51aa19f1c96612de6fe5ccb0d06cb1
“李琦” <qi.li@hongpucorp.com>
2023-06-16 11:01:39 +0800

    feat(资源位置调整): 静态图片使用

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       // },
       alias: {
         "@": resolve("src"),
-        static: resolve("public/static"),
+        // static: resolve("public/static"),
       },
       // 忽略后缀名的配置选项, 添加 .vue 选项时要记得原本默认忽略的选项也要手动写入
       extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
