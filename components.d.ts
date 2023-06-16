9fad141f09b506c683c8143dd921cc58b82f5c94
“李琦” <qi.li@hongpucorp.com>
2023-06-16 11:19:09 +0800

    feat: 文件模拟

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div class="app">
     <el-button type="primary">点击试试</el-button>
     <div>你愁啥</div>
-    <div>nik</div>
-    <div>你猜</div>
     <div>
       svg-icon 图标
       <svg-icon icon-class="fullscreen" className="header-icon" />
@@ -15,6 +13,31 @@
         className="header-icon"
       />
     </el-tooltip>
+    <div class="moni">
+      空中飞行员
+      <span>试试看</span>
+    </div>
+    <div>
+      <img src="@/assets/image/cro-avatar.jpg" alt="" />
+    </div>
+    <el-icon :size="20">
+      <Edit />
+    </el-icon>
+    <el-icon color="#409EFC" class="no-inherit">
+      <Share />
+    </el-icon>
+    <el-icon>
+      <Delete />
+    </el-icon>
+    <el-icon class="is-loading">
+      <Loading />
+    </el-icon>
+    <el-button type="primary">
+      <el-icon style="vertical-align: middle">
+        <Search />
+      </el-icon>
+      <span style="vertical-align: middle"> Search </span>
+    </el-button>
   </div>
 </template>
 <script setup lang="ts">
@@ -39,5 +62,14 @@ const { toggle, isFullscreen } = useFullscreen();
 }
 .svg-icon:focus {
   border: none;
+}
+// 实验代码
+.moni {
+  color: red;
+  font-size: 30px;
+  & span {
+    color: yellow;
+    font-size: 36px;
+  }
 }
 </style>
